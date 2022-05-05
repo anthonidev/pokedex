93289b2fc99d43ceafc00ddcee17ef4a295bcb98
anthonidev <softwaretoni21@gmail.com>
2022-05-05 02:10:28 -0500

    fix

--- src/components/notification/Alert.tsx
+++ src/components/notification/Alert.tsx
@@ -57,59 +57,7 @@ const Alert = () => {
                             </motion.div>
                         </motion.div>
                     )
-                case 'red':
-                    return (
-                        <motion.div variants={stagger} initial="initial" animate="animate" className="fixed bottom-2 z-50 w-1/2 flex flex-row items-center bg-red-200 p-5 rounded border-b-2 border-red-300">
-                            <motion.div variants={fadeInUp} className=" flex items-center bg-red-100 border-2 border-red-500 justify-center h-10 w-10 flex-shrink-0 rounded-full">
-                                <span className="text-red-500">
-                                    <svg fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        className="h-6 w-6">
-                                        <path fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"></path>
-                                    </svg>
-                                </span>
-                            </motion.div>
-                            <motion.div variants={fadeInUp} className="alert-content ml-4">
-                                <div className="alert-title font-semibold text-lg text-red-800">
-                                    Error
-                                </div>
-                                <div className="alert-description text-sm text-red-600">
-                                    {alert.msg}
-                                </div>
-                            </motion.div>
-                        </motion.div>
-                    )
-                case 'blue':
-                    return (
-
-
-                        <motion.div variants={stagger} initial="initial" animate="animate" className="fixed bottom-2 z-50 w-1/2 flex flex-row items-center bg-blue-200 p-5 rounded border-b-2 border-blue-300">
-                            <motion.div variants={fadeInUp} className="flex items-center bg-blue-100 border-2 border-blue-500 justify-center h-10 w-10 flex-shrink-0 rounded-full">
-                                <span className="text-blue-500">
-                                    <svg fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        className="h-6 w-6">
-                                        <path fillRule="evenodd"
-                                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                                            clipRule="evenodd"></path>
-                                    </svg>
-                                </span>
-                            </motion.div>
-                            <motion.div variants={fadeInUp} className="alert-content ml-4">
-                                <div className="alert-title font-semibold text-lg text-blue-800">
-                                    Información
-                                </div>
-                                <div className="alert-description text-sm text-blue-600">
-                                    {alert.msg}
-                                </div>
-                            </motion.div>
-                        </motion.div>
-                    )
-
-                default:
-                    break;
+                
             }
 
         } else {
